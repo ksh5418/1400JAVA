071a4010f0d3cf6f3de875aa5df686a77732ca11
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-08 16:18:33 +0900

    20220708

---
+++ src/Ch02/C02진수서식문자.java
@@ -0,0 +1,31 @@
+package Ch02;
+
+public class C02진수서식문자 {
+
+	public static void main(String[] args) {
+		
+		//접두사
+		//0		: 8진수
+		//0x	: 16진수
+		//0b	: 2진수
+		
+		//10진수
+		System.out.printf("10진수로 변환 : %d\n",0b00001111);
+		System.out.printf("10진수로 변환 : %d\n", 173);//10진수
+		System.out.printf("10진수로 변환 : %d\n", 0255);//8진수
+		System.out.printf("10진수로 변환 : %d\n", 0xAD);//16진수
+
+		//8진수
+		System.out.printf("10진수로 변환 : %o\n", 173);//10진수
+		System.out.printf("10진수로 변환 : %o\n", 0255);//8진수
+		System.out.printf("10진수로 변환 : %o\n", 0xAD);//16진수
+
+		//16진수
+		System.out.printf("10진수로 변환 : %x\n", 173);//10진수
+		System.out.printf("10진수로 변환 : %x\n", 0255);//8진수
+		System.out.printf("10진수로 변환 : %x\n", 0xAD);//16진수
+
+		
+	}
+
+}

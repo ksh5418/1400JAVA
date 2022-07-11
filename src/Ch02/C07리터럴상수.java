db80291a67cc8a0f920ce62278f97aa364cb5be2
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-11 16:50:07 +0900

    20220711

---
+++ src/Ch02/C07리터럴상수.java
@@ -0,0 +1,38 @@
+package Ch02;
+
+public class C07리터럴상수 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//상수 : 고정된 수
+		//상수 종류
+		//리터럴 상수 : 이름부여 X
+		//심볼릭 상수 : 이름부여 O (final 예약어)
+		
+		//정수 리터럴 기본자료형 : int
+		//실수 리터럴 기본자료형 : double
+		
+		
+		//리터럴 접미사 
+		//정수 : l,L (long)
+		//실수 : f,F(float)
+		
+		//예제 1
+//		byte num1 = (byte)129; //byte는 128이상이면 사용 안되서 강제 형변환 사용 (출력시 형변환을 해서 byte의 최대 값으로 출력된다)
+//		long num2 = 123L; 
+//		double num3 = 123.456;
+//		float num4 = 123.456f;
+//		
+//		System.out.println(num1);
+//		
+		//예제 2
+//		int num1 = 2200000000L; // 40억
+		long num2 = 4000000000L; // 40억
+		
+		
+		
+		
+	}
+
+}
---
+++ src/Ch02/C08한문자저장.java
@@ -0,0 +1,24 @@
+package Ch02;
+
+public class C08한문자저장 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println((int)'A');
+		System.out.println((int)'a');
+		System.out.println((int)'가');
+		System.out.println((int)'나');
+		
+		System.out.printf("%x\n", (int)'가');
+		System.out.printf("%x\n", (int)'나');
+		
+		System.out.println('\ub090');
+		System.out.println('\ub0b1');
+		System.out.println('\uac00');
+		System.out.println('\ub098');
+		
+		System.out.println((char)65);
+		
+	}
+
+}

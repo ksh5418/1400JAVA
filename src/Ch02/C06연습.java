f719613cfb6aa9447864e826d2f6c400543f0d9c
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-11 15:07:14 +0900

    20220711

---
+++ src/Ch02/C06연습.java
@@ -0,0 +1,17 @@
+package Ch02;
+
+public class C06연습 {
+
+	public static void main(String[] args) {
+		int a;
+		a = 5;
+		
+		short b = 100;
+		
+		int result = a+b;
+		
+		System.out.println(result);
+		
+	}
+
+}

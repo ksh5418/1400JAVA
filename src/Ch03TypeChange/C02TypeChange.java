a4e7f47e83ecc4185cfe7238ba3e81f648061bc8
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-12 16:34:09 +0900

    20220712

---
+++ src/Ch03TypeChange/C02TypeChange.java
@@ -0,0 +1,43 @@
+package Ch03TypeChange;
+
+public class C02TypeChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//강제 형변환
+		
+		int intval = '가';
+		char charval = (char)intval; //강제형변환
+		System.out.println(charval);
+		
+		long longval = 1000;
+		intval = (int)longval; // 강제형변환
+		System.out.println(intval);
+		
+		double doubleval = 3.14;
+		intval = (int)doubleval; //강제형변환
+		System.out.println(intval);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
